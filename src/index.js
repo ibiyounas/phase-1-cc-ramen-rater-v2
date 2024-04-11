431f310a7a8f03b7ebca1e649a8f8451d30cbd27
Ibrahem Younas <younasibrahem@gmail.com>
2024-04-11 13:04:20 -0400

    runs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,76 @@
 // index.js
 
 // Callbacks
-const handleClick = (ramen) => {
+const perRamen = (ramenObj) => {
+  const menuDiv = document.getElementById("ramen-menu")
+  const img = document.createElement("img")
+  img.src = ramenObj.image
+  img.alt = ramenObj.name
+  img.classList.add("image-slider");
+  img.addEventListener("click", (event) => handleClick(ramenObj, event))
+
+  menuDiv.appendChild(img);
+
+}
+
+const handleClick = (ramenDetail) => {
   // Add code
+  const imgDetail = document.querySelector("#ramen-detail > .detail-image")
+  const nameDetail = document.querySelector("#ramen-detail > .name")
+  const restaurant = document.querySelector("#ramen-detail > .restaurant")
+  const rating = document.getElementById("rating-display")
+  const comment = document.getElementById("comment-display")
+
+  imgDetail.src = ramenDetail.image;
+  imgDetail.alt = ramenDetail.image;
+  nameDetail.innerText = ramenDetail.name;
+  restaurant.innerText = ramenDetail.restaurant;
+  rating.innerText = ramenDetail.rating.toString();
+  comment.innerText = ramenDetail.comment;
+
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const newname = event.target['new-name'].value
+  const newrestaurant = event.target.restaurant.value
+  const newimage = event.target.image.value
+  const newrating = event.target.rating.value
+  const newcomment = event.target['new-comment'].value
+  const newRamen = { newname, newrestaurant, newimage, newrating, newcomment }
+  event.target.reset()
+  perRamen(newRamen);
 };
 
 const addSubmitListener = () => {
   // Add code
+  const Form = document.querySelector("#new-ramen");
+  if (Form) {
+    Form.addEventListener("submit", handleSubmit);
+  }
+  
 }
 
 const displayRamens = () => {
   // Add code
+  fetch("http://localhost:3000/ramens")
+  .then((resp) => resp.json())
+  .then((data) => {
+    document.getElementById("ramen-menu").innerHTML = "";
+    data.forEach(perRamen)
+  })
+  .catch((error) => console.log(error));
 };
+    
 
 const main = () => {
   // Invoke displayRamens here
+  displayRamens();
   // Invoke addSubmitListener here
+  addSubmitListener();
+
+  handleSubmit();
+  handleClick();
 }
 
 main()
@@ -26,4 +81,6 @@ export {
   addSubmitListener,
   handleClick,
   main,
+  perRamen,
+  handleSubmit,
 };
